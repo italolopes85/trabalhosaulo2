3ab5e37c1e79b8df2afe8b6baf23f45e96c2f013
italolopes85 <italo.josue@escolar.ifrn.edu.br>
2025-03-10 16:52:02 -0300

    sauloacabo

---
+++ trabalho2saulo/Assets/Scripts/animacaoJogador.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animacaoJogador : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private Rigidbody2D rigidbody;
+   
+
+   
+    void Update()
+    {
+        float velocidadeX = Mathf.Abs(this.rigidbody.velocity.x);
+        if (velocidadeX > 0)
+        {
+            this.animator.SetBool("Andando", true);
+        }
+        else
+        {
+            this.animator.SetBool("Andando", false);
+        }
+    }
+}
